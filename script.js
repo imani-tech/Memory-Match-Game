9a09d61c178d6208ce9356596b8b0be8302ead6c
Imani muthoni <imanimuthoni9@gmail.com>
2026-05-19 17:48:08 +0300

    displayed the images

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ const movesText = document.getElementById("movesText");
 const matchesText = document.getElementById("matchesText");
 const messageText = document.getElementById("messageText");
 const resetBtn = document.getElementById("resetBtn");
+// an array of images
+const cardValues = ["chicken.jpg", "horse.png", "chicken.jpg", "horse.png"];
+// looop through my array and display each image in an image tag
+cardValues.forEach(function (image){
+let imageContainer = document.createElement("img")
+imageContainer.src = image;
+imageContainer.classList.add("card")
+gameBoard.append(imageContainer)
 
-const cardValues = ["X", "B", "A", "G", "X", "B", "A", "G"];
+})
