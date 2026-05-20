07bbaf448330c0d1889d1c009b0dca36fc6dd1d3
Imani muthoni <imanimuthoni9@gmail.com>
2026-05-20 13:45:38 +0300

    Trying to store the clicked value in a variable

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ const resetBtn = document.getElementById("resetBtn");
 // an array of images
 const cardValues = ["chicken.jpg", "horse.png", "chicken.jpg", "horse.png"];
 // looop through my array and display each image in an image tag
-cardValues.forEach(function (image){       
+cardValues.forEach(function (image){  
+ //  created an image tag
 let imageContainer = document.createElement("img")
+ //gave it a class list 
 imageContainer.classList.add("card")
+// appended it to the game board so it can display
 gameBoard.append(imageContainer)
+// every time the image is clicked you can see whats inside
+let  firstCard  = null
+let secondCard = null
 imageContainer.addEventListener("click", (event) => {
     imageContainer.src = image;
+   if(!firstCard){
+       firstCard = event.target.src
+    }else(
+        secondCard = event.target.src
+    )  
+     
+    console.log(firstCard)
+    console.log(secondCard)
+    
+    
 })
 })
