1f6fd2cf01db29a5a881f9123435b7071af51f93
Imani muthoni <imanimuthoni9@gmail.com>
2026-05-20 19:19:21 +0300

    now it checks if the two images are the same

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const messageText = document.getElementById("messageText");
 const resetBtn = document.getElementById("resetBtn");
 // an array of images
 const cardValues = ["chicken.jpg", "horse.png", "chicken.jpg", "horse.png"];
+let  firstCard  = null
+let secondCard = null
 // looop through my array and display each image in an image tag
 cardValues.forEach(function (image){  
  //  created an image tag
@@ -13,20 +15,20 @@ let imageContainer = document.createElement("img")
 imageContainer.classList.add("card")
 // appended it to the game board so it can display
 gameBoard.append(imageContainer)
+imageContainer.dataset.image = image;
 // every time the image is clicked you can see whats inside
-let  firstCard  = null
-let secondCard = null
 imageContainer.addEventListener("click", (event) => {
+    event.preventDefault()
     imageContainer.src = image;
+    let clickedCard = event.target;
    if(!firstCard){
-       firstCard = event.target.src
+       firstCard = clickedCard
     }else(
-        secondCard = event.target.src
+        secondCard = clickedCard
     )  
-     
-    console.log(firstCard)
-    console.log(secondCard)
-    
+     if(firstCard.dataset.image === secondCard.dataset.image){
+        messageText.textContent = "You got it 🥳"
+     }   
     
 })
 })
