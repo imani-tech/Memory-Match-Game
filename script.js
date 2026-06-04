d27758042930473a42a6892d33bc519567f817be
Imani muthoni <imanimuthoni9@gmail.com>
2026-06-04 16:11:38 +0300

    made sure you cant click more than two images

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const cardValues = ["chicken.jpg", "horse.png", "chicken.jpg", "horse.png"];
 // declared variables that will store the clicked image
 let  firstCard  = null
 let secondCard = null
+let lockBoard = false;
 // looop through my array and display each image in an image tag
 cardValues.forEach(function (image){  
  //  created an image tag
@@ -17,36 +18,54 @@ imageContainer.classList.add("card")
 // appended it to the game board so it can display
 gameBoard.append(imageContainer)
 imageContainer.dataset.image = image;
+
+// **********************Event listener***************
+
 // every time the image is clicked you can see whats inside
-imageContainer.addEventListener("click", (event) => {
+imageContainer.addEventListener("click", function imagdisplay(event) {
     // prevented the page from loading after you click it
-    event.preventDefault()
-    // shows the image when clicked
-    imageContainer.src = image;
+    event.preventDefault()    
+    if(lockBoard){
+        return
+    } 
+    
+    // shows the image when clicked   
     // tells me the exact image clicked 
+
     let clickedCard = event.target;
+    if (clickedCard === firstCard){
+        return
+    } 
+    clickedCard.src = clickedCard.dataset.image; 
+         
     // validates the cards clicked
   if(!firstCard){
-    firstCard = clickedCard
+    firstCard = clickedCard;
+    return
 }else if(!secondCard){
-    secondCard = clickedCard
+    secondCard = clickedCard;
+    lockBoard = true;
 }else{
     return
 }
+
     if(firstCard && secondCard){
 
     if(firstCard.dataset.image === secondCard.dataset.image){
         messageText.textContent = "You got it 🥳"
         
     }else{
-        messageText.textContent = "image didn't match 😔"
+        messageText.textContent = "image didn't match 😔"                  
     }
+    firstCard = null
+    secondCard = null
 
 }
-    console.log(firstCard)
-    console.log(secondCard)   
-   
+
+
 
 })
-})
 
+})
+lockBoard = false;
+console.log(lockBoard)
