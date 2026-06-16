ad231f3b913ae3dd0d696355c326f7c5e01cd27b
Imani muthoni <imanimuthoni9@gmail.com>
2026-06-16 19:14:53 +0300

    added semi colons but there are errors

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const resetBtn = document.getElementById("resetBtn");
 // an array of images
 const cardValues = ["chicken.jpg", "horse.png", "chicken.jpg", "horse.png", "cat photo.jpg", "dog photo.jpg", "cat photo.jpg", "dog photo.jpg"];
 // declared variables that will store the clicked image
-let  firstCard  = null
-let secondCard = null
+let  firstCard  = null;
+let secondCard = null;
 let lockBoard = false;
 let hideImage = false
 let moves = 0;
@@ -18,86 +18,92 @@ let SELECTED_IMG_EQUAL = [];
 // looop through my array and display each image in an image tag
 cardValues.forEach(function (image){  
     //  created an image tag
-    let imageContainer = document.createElement("img")
+    let imageContainer = document.createElement("img");
     //gave it a class list 
-    imageContainer.classList.add("card")
+    imageContainer.classList.add("card");
     // appended it to the game board so it can display
-    gameBoard.append(imageContainer)
+    gameBoard.append(imageContainer);
     // stores some information on the HTML element.
     imageContainer.dataset.image = image;
-    imageContainer.src = "download (4).jpg" 
+    imageContainer.src = "download (4).jpg" ;
 // **********************Event listener***************
 
 // every time the image is clicked you can see whats inside
 imageContainer.addEventListener("click", function imagdisplay(event) {
     // prevented the page from loading after you click it
-    event.preventDefault()   
+    event.preventDefault();   
     // It stops the user clicking when it is true 
     if(lockBoard){
         return
-    }        
+    } ;       
     // tells me the exact image clicked 
     let clickedCard = event.target;
     // makes sure no clicking the same image
     if (clickedCard === firstCard){
         return
-    } 
+    } ;
     // shows the image when clicked 
     clickedCard.src = clickedCard.dataset.image;        
     // validates the cards clicked
     if(!firstCard){
         firstCard = clickedCard;
         // after the first card is selcted its stored in selectedCards array
-        selectedCards.push(firstCard)
-        return
+        selectedCards.push(firstCard);
+        return;
     }else if(!secondCard){
         secondCard = clickedCard;
         // after the second card is selcted its stored in selectedCards array
-        selectedCards.push(secondCard)
+        selectedCards.push(secondCard);
         // stoped the image from showing when clicking
         lockBoard = true;        
-    }
+    }; 
+   
 // makes sure to count moves after 2 images are selected
     if(selectedCards.length >= 2){
-        moves++
-        movesText.textContent = moves
-    } 
+        moves++;
+        movesText.textContent = moves ;      
+    } ;
+    
     // validates if the two images selected are equal
     if(firstCard && secondCard){
-
     if(firstCard.dataset.image === secondCard.dataset.image){
-        messageText.textContent = "You got it 🥳"
+        messageText.textContent = "You got it 🥳";
         // keeps truck of the selected equal images
-        SELECTED_IMG_EQUAL.push(firstCard, secondCard)
+        SELECTED_IMG_EQUAL.push(firstCard, secondCard);
          // to aviod already selected images to be clicked again
-        firstCard.classList.add("open-image-locker") 
-        secondCard.classList.add("open-image-locker")        
+        firstCard.classList.add("open-image-locker"); 
+        secondCard.classList.add("open-image-locker");             
     } else{
-        messageText.textContent = "Image didn't match 😔"        
+        messageText.textContent = "Image didn't match 😔";  
+        SELECTED_IMG_EQUAL = [];      
         }
+      
     //   after the first two images are selected enable selection of the rest  
-        if(SELECTED_IMG_EQUAL.length === 2){
+        if(SELECTED_IMG_EQUAL.length >= 2){            
             setTimeout(() => {
-            messageText.textContent = "Find the next one 😉👍"
-            firstCard = null
-            secondCard = null 
+            messageText.textContent = "Find the next one 😉👍";
+            firstCard = null;
+            secondCard = null; 
             lockBoard = false;  
-            return          
-        }, 1500);            
-        }          
+            console.log(SELECTED_IMG_EQUAL)            
+             return;     
+        }, 1500);                   
+        }; 
+         if(SELECTED_IMG_EQUAL.length === 8){
+            messageText.textContent = "congrats🎉 you found them all🎊👏";
+         };   
         if(firstCard.dataset.image !== secondCard.dataset.image){
             setTimeout(() => {
-                firstCard.src = "download (4).jpg"
-                secondCard.src = "download (4).jpg"  
-
-                firstCard = null
-                secondCard = null  
+                messageText.textContent = "Please try again👍" ; 
+                firstCard.src = "download (4).jpg";
+                secondCard.src = "download (4).jpg";  
+                firstCard = null;
+                secondCard = null;                  
                 lockBoard = false;
-                messageText.textContent = "Please try again👍"   
-            }, 1500)
-        }   
-    }
-  
+                
+            }, 1100)
+        };   
+    }   
 })   
 })
 resetBtn.addEventListener("click", () => {
